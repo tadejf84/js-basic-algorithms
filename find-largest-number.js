2bf60bb6cf6dd310ae1157513c4ffdc20755add2
tadej <tadej@mail.com>
2019-02-03 11:16:38 +0100

    find largest number in array

--- find-largest-number.js
+++ find-largest-number.js
@@ -1,17 +1,13 @@
+/*
+* find max value in array
+*/
 function findMax(arr) {
   let max = 0;
 
-  /* using a for loop */
-  for (let i of arr) {
-    if (i > max) {
-      max = i;
-    }
-  }
+  // using math max and spread operator
+  max = Math.max(...arr);
 
-  /* using math max and spread operator */
-  // max = Math.max(...arr);
-
-  /* using a map method */
+  // using a map method 
   // arr.map( num => {
   //   if (num > max){
   //     max = num;
@@ -21,7 +17,3 @@ function findMax(arr) {
   return max;
 
 }
-
-var para = document.querySelector('p');
-var exampleArr = [6, 7, 111, 12177, 227, 13, 1890, 11];
-// para.innerHTML = findMax(exampleArr);
