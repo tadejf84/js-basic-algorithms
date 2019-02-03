e2bd425580d9681f2cf707ca7f19708bb59942cb
tadej <tadej@mail.com>
2019-02-03 13:23:46 +0100

    filter array for numbers only

--- is-number.js
+++ is-number.js
@@ -1,23 +1,9 @@
+/*
+* filter array for numbers
+*/
 function isNumber(arr) {
-  let onlyNum = [];
-
-  /* using a while loop */
-  // let i = 0;
-  // while (i < arr.length) {
-  //   if (typeof arr[i] === "number") {
-  //     onlyNum.push(arr[i]);
-  //   }
-  //   i++;
-  // }
-
-  /* using the array filter method*/
-  onlyNum = arr.filter(el => {
+  return arr.filter(el => {
     return typeof el === "number";
   });
-
-  return onlyNum;
 }
 
-var para = document.querySelector('p');
-var exampleArr = ["str", true, 33, 26, "test", 24, "6", 11, null, 'test2', 22];
-// para.innerHTML = isNumber(exampleArr);
