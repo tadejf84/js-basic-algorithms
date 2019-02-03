5fc30c9c9b6c7ae7c0fb943c47b45dc4b9364dd8
tadej <tadej@mail.com>
2019-02-03 13:09:26 +0100

    find greatest common divisor of two nums

--- greatest-common-divisor.js
+++ greatest-common-divisor.js
@@ -1,11 +1,18 @@
+/*
+* find greatest common divisor of two numbers
+*/
 function greatestCommonDiv(num1, num2) {
   let div = 0;
 
-  /* check user input */
+  // check if input not a number
   if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    return 'Input not a number!';
+    return 'Input is not a number!';
   }
-  if (num1 === 0 || num2 === 0) { div = 'null'; }
+
+  // check if any of the inputs is equal to 0
+  if (num1 === 0 || num2 === 0) { 
+    return num1 >= num2 ? num1 : num2;
+  }
 
   for (let i = 0; i <= num1; i++) {
     if (num1%i === 0 && num2%i === 0) {
@@ -15,5 +22,3 @@ function greatestCommonDiv(num1, num2) {
   return div;
 }
 
-var para = document.querySelector('p');
-// para.innerHTML = greatestCommonDiv(7, 21);
