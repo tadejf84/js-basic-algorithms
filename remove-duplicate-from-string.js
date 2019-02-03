8f9007247033d3c7f49d3e33f8791ec9a9293f8a
tadej <tadej@mail.com>
2019-02-03 14:30:32 +0100

    remove duplicate chars from string

--- remove-duplicate-from-string.js
+++ remove-duplicate-from-string.js
@@ -1,15 +1,15 @@
+/*
+* remove duplicate characters from string
+*/
 function removeDuplicateLettersFromString(str) {
   let el,
       exists = {},
       output = [];
 
-  /* only alpha numerical, to lowercase, split to array */
+  // only alpha numerical, to lowercase, split to array
   str = str.replace(/[^a-z0-9]/gi,'').toLowerCase().split('');
 
-  /*
-  loop through the array
-  if element does not exist, push into output array
-  */
+  // push only unique chars to arr
   for (let i of str) {
     el = i;
     if(!exists[el]) {
@@ -18,10 +18,6 @@ function removeDuplicateLettersFromString(str) {
     }
   }
 
-  /* join array to string */
   return output.join('');
 }
 
-var para = document.querySelector('p');
-var example = 'this is an example sentence';
-// para.innerHTML = removeDuplicateLettersFromString(example);
