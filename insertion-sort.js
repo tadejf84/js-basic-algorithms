f8d3b0b590123cf581e21bba47f7e2e3ada3ea8b
tadej <tadej@mail.com>
2019-02-03 13:18:54 +0100

    insertion sort array algorithm

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,20 +1,16 @@
+/*
+* insertion sort array
+*/
 function insertionSort(arr){
-  let el, i, j;
-  const len = arr.length;
 
-  for(i = 1; i < len; i++){
-    el = arr[i];
-    j = i;
-    while(j > 0 && arr[j-1] > el){
-      arr[j] = arr[j-1];
-      j--;
-   }
-   arr[j] = el;
-  }
+  arr.forEach( (el, i) => {
+    while(i > 0 && arr[i - 1] > el){
+      arr[i] = arr[i - 1];
+      i--;
+    }
+    arr[i] = el;
+  });
 
   return arr;
 }
 
-var para = document.querySelector('p');
-var example = [12, 11, 7, 13, 14, 16, 2, 22];
-// para.innerHTML = insertionSort(example);
