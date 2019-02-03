f4957ba8560426e2fea66fa217bbbdd9310866a3
tadej <tadej@mail.com>
2019-02-03 11:41:24 +0100

    find missing number in array

--- find-missing-number.js
+++ find-missing-number.js
@@ -1,12 +1,13 @@
-/* find missing number with sorting */
+/*
+* find missing number with sorting 
+*/
 function findMissingNum1(arr) {
-  let sorted = [];
+
+  // sort the array
+  const sorted = arr.sort();
   const n = arr.length + 1;
 
-  /* sort the array */
-  sorted = arr.sort();
-
-  /* return the missing number in the arr */
+  // return the missing number in the arr
   for (let i = 0; i < n; i++) {
     if(sorted[i] + 2  === sorted[i+1]) {
       return sorted[i+1] - 1;
@@ -14,14 +15,16 @@ function findMissingNum1(arr) {
   }
 }
 
-/* find missing number without sorting */
+/* 
+* find missing number without sorting 
+*/
 function findMissingNum2(arr) {
   const n = arr.length + 1;
 
-  /* formula for expected sum of arr length + 1 */
+  // formula for expected sum of arr length + 1
   const expectedSum = n * (n + 1) / 2;
 
-  /* get actual sum of all elements in the arr */
+  // get actual sum of all elements in the arr
   const actualSum = arr.reduce((a, b) => {
     return a + b;
   }, 0);
@@ -29,6 +32,3 @@ function findMissingNum2(arr) {
   return expectedSum - actualSum;
 }
 
-var para = document.querySelector('p');
-// para.innerHTML = findMissingNum1([1,2,4,3,5,6,9,8,10]);
-// para.innerHTML = findMissingNum2([1,2,4,3,5,6,9,8,10]);
