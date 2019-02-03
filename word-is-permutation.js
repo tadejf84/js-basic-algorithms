95897ff076144cdb7e1d4c9eb83c341e35408a54
tadej <tadej@mail.com>
2019-02-03 15:02:41 +0100

    is word permutation of another word

--- word-is-permutation.js
+++ word-is-permutation.js
@@ -1,8 +1,7 @@
+/*
+* is word permutation of another word
+*/
 function isPermutation(str1, str2) {
-
   return str1.split('').sort().join('') === str2.split('').sort().join('');
-
 }
 
-var para = document.querySelector('p');
-// para.innerHTML = isPermutation('test', 'tset');
