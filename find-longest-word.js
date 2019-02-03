d53fb0c5e5a2e4fbf712a31a2548083a12692812
tadej <tadej@mail.com>
2019-02-03 11:28:59 +0100

    find the longest word in a string

--- find-longest-word.js
+++ find-longest-word.js
@@ -1,27 +1,21 @@
+/*
+* find longest word in a sentence
+*/
 function longestWord(str) {
 
-	/* filter array for alpha numeric characters only */
+	// filter array for alpha numeric characters only
 	const words = str.toLowerCase().match(/[a-z0-9]+/g);
 
-	/* sort words by length */
+	// sort words by length
 	const sorted = words.sort((a,b) => {
 		return b.length - a.length;
 	});
 
-	/* return one or more words with most characters */
+	// return one or more words with most characters
 	const longestWords = sorted.filter((word) => {
 		return word.length === sorted[0].length;
 	});
 
-	if (longestWords.length > 1) {
-		return 'The longest words are: ' + longestWords + ' and are ' +
-		longestWords[0].length + ' characters long.';
-	} else {
-		return 'The longest word is: ' + longestWords + ' and is ' +
-		longestWords[0].length + ' characters long.';
-	}
-
+	return longestWords;
 }
 
-var para = document.querySelector('p');
-// para.innerHTML = longestWord('Test sentence once again');
