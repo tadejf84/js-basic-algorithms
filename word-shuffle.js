2ac95e81b1ea15c0e94e51df95156b9c0fa658a3
tadej <tadej@mail.com>
2019-02-03 15:07:33 +0100

    word shuffle in string

--- word-shuffle.js
+++ word-shuffle.js
@@ -1,21 +1,18 @@
+/*
+* shuffle words in sentence
+*/
 function wordShuffle(str) {
   let wordShuffle = []
       words = [];
 
-  /* split string into words array */
+  // split string into words array
   words = str.split(' ');
 
-  /*
-  loop trough the words array in the opposite direction
-  push into wordShuffle array
-  */
+  // loop trough the words array in the opposite direction and push into wordShuffle array
   for (let i = words.length; i >= 0; i--) {
     wordShuffle.push(words[i]);
   }
 
-  /* join output array to string */
   return wordShuffle.join(' ');
 }
 
-var para = document.querySelector('p');
-// para.innerHTML = wordShuffle('test this awesome sentence');
