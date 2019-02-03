f9b4a96083252c786214f25b5c3a2d02fc09d2c9
tadej <tadej@mail.com>
2019-02-03 14:53:16 +0100

    convert string to integer

--- string-to-integer.js
+++ string-to-integer.js
@@ -1,19 +1,13 @@
+/*
+* transform string to integer
+*/
 function stringToInt(str) {
   let sum = 0;
   const arr = str.split('');
 
-  // using a for loop
-  // for (let i = 0; i < arr.length; i++) {
-  //   sum += arr[i] * Math.pow(10, arr.length - i - 1);
-  // }
-
-  // using map method
   arr.map((currElement, index) => {
     sum += currElement * Math.pow(10, arr.length - index - 1);
   });
 
-  return sum + ' is a ' + typeof sum;
+  return sum;
 }
-
-var para = document.querySelector('p');
-// para.innerHTML = stringToInt('105');
