356e5e538d6f460c6443573a7ff2fb774e0effbf
tadej <tadej@mail.com>
2019-02-03 14:17:27 +0100

    find character with most occurences in a string

--- max-characters.js
+++ max-characters.js
@@ -1,18 +1,17 @@
+/*
+* get char with most occurence in string
+*/
 function maxChar(str){
   let charCount = {},
       maxCount = 0,
       maxIndex;
 
-  /* string to lower case, only alphanumerical, split to array */
-  let strArr = str.toLowerCase()
+  // string to lower case, only alphanumerical, split to array
+  const strArr = str.toLowerCase()
                   .replace(/[^a-z0-9]/gi,'')
                   .split('');
 
-  /*
-  loop through the string array
-  increment by one if char already exists in charCount
-  else initialize to 1
-  */
+  // count chars
   for(let char of strArr) {
     if (charCount[char]) {
       charCount[char]++;
@@ -21,18 +20,14 @@ function maxChar(str){
     }
   }
 
-  /*
-  loop through the charCount object,
-  look for the char with the highest count
-  */
+  // find highest char count
   for (let char in charCount) {
     if (charCount[char] > maxCount) {
       maxCount = charCount[char];
       maxIndex = char;
     }
   }
-  return 'The character with the most frequent occurence is ' + maxIndex + ' and can be found ' + maxCount + ' times in this example!';
+
+  return [maxIndex, maxCount];
 }
 
-var para = document.querySelector('p');
-// para.innerHTML = maxChar('Houston, we have a problem!');
