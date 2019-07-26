35881b4f2ff0e024342058160fa65371aebab042
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2019-07-26 14:04:39 +0200

    swap two numbers

--- swap-number.js
+++ swap-number.js
@@ -10,9 +10,12 @@ function swapNum(a, b) {
   // b = temp;
 
   // without temp
-  b = b - a;
-  a = a + b;
-  b = a - b;
+  // b = b - a;
+  // a = a + b;
+  // b = a - b;
+
+  // es6 deconstructuring
+  [a,b] = [b,a];
 
   return [a, b];
 }
