c902fc47ac8ad3d6898c684a6b23e0e39c339e17
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2019-07-26 09:12:51 +0200

    find duplicates letters in a string

--- find-duplicate-letters.js
+++ find-duplicate-letters.js
@@ -3,6 +3,9 @@
 */
 function findDuplicate(str) {
 
+  // initial input check
+  if (str.length === 0) return 'You entered an empty string!';
+
   // get only alphanumeric chars, to lower case, sort and split string to array
   const letter = str.replace(/[^a-z0-9]/gi,'')
                 .toLowerCase()
