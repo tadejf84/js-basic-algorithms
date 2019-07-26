524e499a974c17e67273b47fc2c8073ef07010a0
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2019-07-26 09:14:38 +0200

    find largest number in array

--- find-largest-number.js
+++ find-largest-number.js
@@ -2,18 +2,17 @@
 * find max value in array
 */
 function findMax(arr) {
-  let max = 0;
 
   // using math max and spread operator
-  max = Math.max(...arr);
+  return Math.max(...arr);
 
   // using a map method 
+  // let max = 0;
   // arr.map( num => {
   //   if (num > max){
   //     max = num;
   //   }
   // });
-
-  return max;
+  // return max;
 
 }
