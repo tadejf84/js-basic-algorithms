bb6102960a135c3b06488463d52d531c127d36e1
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2019-07-26 09:02:05 +0200

    bubble sort array algorithm

--- bubble-sort.js
+++ bubble-sort.js
@@ -4,6 +4,10 @@
 function bubbleSort(arr){
   const len = arr.length;
 
+  // initial checks
+  if(len === 0) return 'Array is empty!';
+  if(len === 1) return arr;
+
   for (let i = len - 1; i >= 0; i--){
     for(let j = 0; j <= i; j++) {
       if(arr[j-1] > arr[j]) {
