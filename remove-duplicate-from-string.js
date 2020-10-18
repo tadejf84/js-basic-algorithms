57cea23661a9c67d4ce56c63eeab941479e105c8
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 11:28:57 +0200

    remove duplicate characters from string

--- remove-duplicate-from-string.js
+++ remove-duplicate-from-string.js
@@ -1,23 +1,27 @@
-/*
-* remove duplicate characters from string
-*/
+/**
+ * Remove duplicate characters from string
+ * 
+ * @param {string} str 
+ * 
+ * @returns {string} unique chars
+ */
 function removeDuplicateLettersFromString(str) {
-  let el,
-      exists = {},
-      output = [];
+    let el,
+        exists = {},
+        output = [];
 
-  // only alpha numerical, to lowercase, split to array
-  str = str.replace(/[^a-z0-9]/gi,'').toLowerCase().split('');
+    // Only alpha numerical, to lowercase, split to array
+    str = str.replace(/[^a-z0-9]/gi,'').toLowerCase().split('');
 
-  // push only unique chars to arr
-  for (let i of str) {
-    el = i;
-    if(!exists[el]) {
-      output.push(el);
-      exists[el] = true;
+    // Push only unique chars to arr
+    for (let i of str) {
+        el = i;
+        if(!exists[el]) {
+            output.push(el);
+            exists[el] = true;
+        }
     }
-  }
 
-  return output.join('');
+    return output.join('');
 }
 
