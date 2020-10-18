a1d5c03d0955399f102a7003a80ce43b43ea550d
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 10:10:01 +0200

    find largest binary gap

--- largest-binary-gap.js
+++ largest-binary-gap.js
@@ -1,26 +1,34 @@
-/*
-* find largest gap in a binary number
-* longest sequence of 0 between 1
-*/
+/**
+ * Find largest gap in a binary number
+ * Longest sequence of 0 between 1
+ * 
+ * @param {number} num 
+ * 
+ * @returns {number} gap
+ */
 function largestBinaryGap(num) {
-	// convert number into binary
-	const binary = Math.abs(num).toString(2);
 
-	let finalMaxGap = 0,
-		curMaxGap;
+    // Convert number into binary
+    const binary = Math.abs(num).toString(2);
 
-	// loop through binary
-	// if number is 0 increase current gap
-	// if number is 1 compare current gap with previous, reset current max gap
-	for (let digit of binary) {
-		if (digit === '0') {
-			curMaxGap++;
-		} else {
-			if (curMaxGap > finalMaxGap) {
-				finalMaxGap = curMaxGap;
-			}
-			curMaxGap = 0;
-		}
-	}
-	return finalMaxGap;
+    let finalMaxGap = 0,
+        curMaxGap;
+
+    /*
+    * Loop through binary
+    * If number is 0 increase current gap
+    * If number is 1 compare current gap with previous, reset current max gap
+    */
+    for (let digit of binary) {
+        if (digit === '0') {
+            curMaxGap++;
+        } else {
+            if (curMaxGap > finalMaxGap) {
+                finalMaxGap = curMaxGap;
+            }
+            curMaxGap = 0;
+        }
+    }
+
+    return finalMaxGap;
 }
