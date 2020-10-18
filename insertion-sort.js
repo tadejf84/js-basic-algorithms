0ad1294f69740049a2c1a91730e4d057237a2511
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 10:04:59 +0200

    insertion sort algorithm

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,16 +1,20 @@
-/*
-* insertion sort array
-*/
+/**
+ * Insertion sort algorithm
+ * 
+ * @param {array} arr
+ * 
+ * @returns {array} sorted array 
+ */
 function insertionSort(arr){
 
-  arr.forEach( (el, i) => {
-    while(i > 0 && arr[i - 1] > el){
-      arr[i] = arr[i - 1];
-      i--;
-    }
-    arr[i] = el;
-  });
+    arr.forEach( (el, i) => {
+        while(i > 0 && arr[i - 1] > el){
+            arr[i] = arr[i - 1];
+            i--;
+        }
+        arr[i] = el;
+    });
 
-  return arr;
+    return arr;
 }
 
