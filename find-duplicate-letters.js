2da73db85989ae4ce4b1acf3bb242145d347e265
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 09:48:17 +0200

    find duplicate letters in a string

--- find-duplicate-letters.js
+++ find-duplicate-letters.js
@@ -1,21 +1,24 @@
-/*
-* find duplicate letters in a string
-*/
+/**
+ * Find duplicate letters in a string
+ * 
+ * @param {string} str 
+ * 
+ * @returns {array} duplicate letters
+ */
 function findDuplicate(str) {
 
-  // initial input check
-  if (str.length === 0) return 'You entered an empty string!';
+    if (str.length === 0) return 'You entered an empty string!';
 
-  // get only alphanumeric chars, to lower case, sort and split string to array
-  const letter = str.replace(/[^a-z0-9]/gi,'')
-                .toLowerCase()
-                .split('')
-                .sort();
+    // Get only alphanumeric chars, to lower case, sort and split string to array
+    const letter = str.replace(/[^a-z0-9]/gi,'')
+                      .toLowerCase()
+                      .split('')
+                      .sort();
 
-  // loop through the arr and return duplicate values
-  return letter.filter((el,i) => {
-    return el === letter[i+1];
-  });
+    // Loop through the arr and return duplicate values
+    return letter.filter((el,i) => {
+        return el === letter[i+1];
+    });
 
 }
 
