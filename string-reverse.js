a9721349fcb25bf266d9b482511610fe2e69b7ff
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 11:39:18 +0200

    reverse a string

--- string-reverse.js
+++ string-reverse.js
@@ -1,20 +1,24 @@
-/*
-* reverse a string
-*/
+/**
+ * Reverse a string
+ * 
+ * @param {string} str
+ * 
+ * @returns {string} reversed 
+ */
 function stringReverse(str) {
-  let output = '';
-  const len = str.length;
+    let output = '';
+    const len = str.length;
 
-  // initial check of user input
-  if (typeof str !== 'string' ) return 'Invalid input! Not a string.';
-  if (!str) return 'Invalid input! Empty string.';
-  if (len < 2 ) return str;
+    // Initial check of user input
+    if (typeof str !== 'string' ) return 'Invalid input! Not a string.';
+    if (!str) return 'Invalid input! Empty string.';
+    if (len < 2 ) return str;
 
-  // loop through in opposite direction
-  for (var i = len - 1; i >= 0; i--) {
-    output += str[i];
-  }
+    // Loop through in opposite direction
+    for (var i = len - 1; i >= 0; i--) {
+        output += str[i];
+    }
 
-  return output;
+    return output;
 }
 
