072a30ea4e9ec6097b190a34bfede26cd62d1821
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 09:35:34 +0200

    split array into chunks

--- array-chunking.js
+++ array-chunking.js
@@ -1,21 +1,22 @@
-/*
-* split array into chunks
-*/
+/**
+ * Split array into chunks
+ * 
+ * @param {array} arr 
+ * @param {number} len 
+ * 
+ * @returns {array} chunks
+ */
 function arrChunk(arr, len) {
-  const chunkedArr = [];
-  let i = 0;
+    const chunkedArr = [];
+    let i = 0;
 
-  // slice the array into chunks of length len
-  while(i < arr.length) {
-    chunkedArr.push(arr.slice(i, i + len));
-    i += len;
-  }
+    // Slice the array into chunks
+    while(i < arr.length) {
+        chunkedArr.push(arr.slice(i, i + len));
+        i += len;
+    }
 
-  // loop through the array and return chunks
-  return chunkedArr.map((arr, index) => {
-    return 'Chunk ' + (index + 1) + ': [' + arr + ']';
-  });
-
+    return chunkedArr;
 }
 
 
