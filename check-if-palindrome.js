54a417a7c25e0947051831667a6b007bb760728a
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 09:40:49 +0200

    check if a word is a palindrome

--- check-if-palindrome.js
+++ check-if-palindrome.js
@@ -1,23 +1,26 @@
-/*
-* PALINDROME
-* a word, phrase, or sequence that reads the same backwards as forwards
-*/
+/**
+ * Check if a word is a palindrom
+ * Palindrom is a word, phrase, or sequence that reads the same backwards as forwards
+ * 
+ * @param {string} str 
+ * 
+ * @returns {boolean}
+ */
 function checkIfPalindrome(str) {
-  let strSplit = [],
-      strReverse = [];
+    let strSplit = [],
+        strReverse = [];
 
-  // initial input check
-  if ( str.length < 1 ) return 'Invalid input!';
-  if ( str.length === 1) return true;
+    if ( str.length < 1 ) return 'Invalid input!';
+    if ( str.length === 1) return true;
 
-  // get only alphanumeric chars, to lower case and split string to array
-  str = str.replace(/[^a-z0-9]/gi,'').toLowerCase();
-  strSplit = str.split('');
+    // Get only alphanumeric chars, to lower case and split string to array
+    str = str.replace(/[^a-z0-9]/gi,'').toLowerCase();
+    strSplit = str.split('');
 
-  // reverse string
-  strReverse = strSplit.reverse();
+    // Reverse string
+    strReverse = strSplit.reverse();
 
-  // compare the two strings
-  return str === strReverse.join('') ? true : false;
+    // Compare the two strings
+    return str === strReverse.join('') ? true : false;
 }
 
