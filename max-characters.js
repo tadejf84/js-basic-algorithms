8608e29d67dfe4607189de060d6ecfb560634580
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 11:17:19 +0200

    find character with most occurences in a string

--- max-characters.js
+++ max-characters.js
@@ -1,33 +1,37 @@
-/*
-* get char with most occurence in string
-*/
+/**
+ * Get character with most occurences in a string
+ * 
+ * @param {string} str 
+ * 
+ * @returns {array} character and count of occurences
+ */
 function maxChar(str){
-  let charCount = {},
-      maxCount = 0,
-      maxIndex;
+    let charCount = {},
+        maxCount = 0,
+        maxIndex;
 
-  // string to lower case, only alphanumerical, split to array
-  const strArr = str.toLowerCase()
-                  .replace(/[^a-z0-9]/gi,'')
-                  .split('');
+    // String to lower case, only alphanumerical, split to array
+    const strArr = str.toLowerCase()
+                      .replace(/[^a-z0-9]/gi,'')
+                      .split('');
 
-  // count chars
-  for(let char of strArr) {
-    if (charCount[char]) {
-      charCount[char]++;
-    } else {
-      charCount[char] = 1;
+    // Count chars
+    for(let char of strArr) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
     }
-  }
 
-  // find highest char count
-  for (let char in charCount) {
-    if (charCount[char] > maxCount) {
-      maxCount = charCount[char];
-      maxIndex = char;
+    // Find highest char count
+    for (let char in charCount) {
+        if (charCount[char] > maxCount) {
+            maxCount = charCount[char];
+            maxIndex = char;
+        }
     }
-  }
 
-  return [maxIndex, maxCount];
+    return [maxIndex, maxCount];
 }
 
