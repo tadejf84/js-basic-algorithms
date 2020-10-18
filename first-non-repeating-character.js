11b9dfb460ec8bd31a0997683b7ebb00957a258d
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 10:01:45 +0200

    find first non repeating character in a string

--- first-non-repeating-character.js
+++ first-non-repeating-character.js
@@ -1,24 +1,29 @@
-/*
-* find first non repeating character in string
-*/
+/**
+ * Find first non repeating character in a string
+ * 
+ * @param {string} str 
+ * 
+ * @returns {string} char
+ */
 function firstNonRepeatingChar(str) {
-  let char, count = {};
+    let char, 
+        count = {};
 
-  // loop through string arr and increment by 1 if exists, else init value to 1
-  for (let i in str) {
-    char = str[i];
-    if (!count[char]) {
-      count[char] = 1;
-    } else {
-      count[char]++;
+    // Loop through string arr and increment by 1 if exists, else init value to 1
+    for (let i in str) {
+        char = str[i];
+        if (!count[char]) {
+            count[char] = 1;
+        } else {
+            count[char]++;
+        }
     }
-  }
 
-  // if count is 1 return the first not repeating char
-  for (let j in count) {
-    if (count[j] === 1) {
-      return j;
+    // If count is 1 return the first not repeating char
+    for (let j in count) {
+        if (count[j] === 1) {
+            return j;
+        }
     }
-  }
 }
 
