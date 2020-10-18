fef04fcabf8cc3ececfe91ffc6ae4b3870480fde
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 10:03:43 +0200

    find greatest common divisor of two numbers

--- greatest-common-divisor.js
+++ greatest-common-divisor.js
@@ -1,24 +1,28 @@
-/*
-* find greatest common divisor of two numbers
-*/
+/**
+ * Find greatest common divisor of two numbers
+ * 
+ * @param {number} num1 
+ * @param {number} num2 
+ * 
+ * @returns {number} common divisor
+ */
 function greatestCommonDiv(num1, num2) {
-  let div = 0;
+    let div = 0;
 
-  // check if input not a number
-  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    return 'Input is not a number!';
-  }
+    // Check if input not a number
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') return 'Input is not a number!';
 
-  // check if any of the inputs is equal to 0
-  if (num1 === 0 || num2 === 0) { 
-    return num1 >= num2 ? num1 : num2;
-  }
-
-  for (let i = 0; i <= num1; i++) {
-    if (num1%i === 0 && num2%i === 0) {
-      div = i;
+    // Check if any of the inputs is equal to 0
+    if (num1 === 0 || num2 === 0) { 
+        return num1 >= num2 ? num1 : num2;
     }
-  }
-  return div;
+
+    for (let i = 0; i <= num1; i++) {
+        if (num1%i === 0 && num2%i === 0) {
+            div = i;
+        }
+    }
+    
+    return div;
 }
 
