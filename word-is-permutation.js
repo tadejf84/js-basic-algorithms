57338083405aeeeecabf716d4c907a484c78fe4e
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 11:44:15 +0200

    check if one word is a permutation of another word

--- word-is-permutation.js
+++ word-is-permutation.js
@@ -1,7 +1,12 @@
-/*
-* is word permutation of another word
-*/
+/**
+ * Check if one word is a permutation of another word
+ * 
+ * @param {string} str1 
+ * @param {string} str2 
+ * 
+ * @returns {boolean}
+ */
 function isPermutation(str1, str2) {
-  return str1.split('').sort().join('') === str2.split('').sort().join('');
+    return str1.split('').sort().join('') === str2.split('').sort().join('');
 }
 
