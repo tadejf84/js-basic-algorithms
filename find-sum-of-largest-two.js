e48e3232ec4928f8fc4c2aadd4d5ddf5450147aa
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 09:59:50 +0200

    find sum of two largest numbers in an array

--- find-sum-of-largest-two.js
+++ find-sum-of-largest-two.js
@@ -1,27 +1,35 @@
-/*
-* find sum of two largest numbers in array
-*/
+/**
+ * Find sum of the two largest numbers in array
+ * 
+ * @param {array} arr 
+ * 
+ * @returns {number} sum of two
+ */
 function findSumOfTwo(arr) {
-  let largest = 0,
-      second = 0;
-  const len = arr.length;
+    let largest = 0,
+        second = 0;
+    const len = arr.length;
 
-  // check initial input
-  if (len < 2) { return 'Your array is too short!'; }
+    if (len < 2) { return 'Your array is too short!'; }
 
-  for (let el of arr) {
-    // check if arr element is a number
-    if (typeof el === 'number') {
-      // get 2 largest numbers
-      if (el >= largest) {
-        second = largest;
-        largest = el;
-      } else if (el >= second) {
-        second = el;
-      } else {
-        continue;
-      }
+    for (let el of arr) {
+
+        // Check if arr element is a number
+        if (typeof el === 'number') {
+
+            // Get 2 largest numbers
+            if (el >= largest) {
+                second = largest;
+                largest = el;
+            } else if (el >= second) {
+                second = el;
+            } else {
+                continue;
+            }
+
+        }
+
     }
-  }
-  return largest + second;
+
+    return largest + second;
 }
