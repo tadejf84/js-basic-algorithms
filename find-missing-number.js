8067715f2fe6a14f59cfdadd60733bb436bbca82
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 09:55:15 +0200

    find missing number in a given array

--- find-missing-number.js
+++ find-missing-number.js
@@ -1,34 +1,43 @@
-/*
-* find missing number with sorting 
-*/
+/**
+ * Find missing number with sorting
+ * 
+ * @param {array} arr 
+ * 
+ * @returns {number} missing num
+ */
 function findMissingNum1(arr) {
 
-  // sort the array
-  const sorted = arr.sort();
-  const n = arr.length + 1;
+    // Sort the array
+    const sorted = arr.sort();
+    const n = arr.length + 1;
 
-  // return the missing number in the arr
-  for (let i = 0; i < n; i++) {
-    if(sorted[i] + 2  === sorted[i+1]) {
-      return sorted[i+1] - 1;
+    // Return the missing number in the arr
+    for (let i = 0; i < n; i++) {
+        if(sorted[i] + 2  === sorted[i+1]) {
+            return sorted[i+1] - 1;
+        }
     }
-  }
 }
 
-/* 
-* find missing number without sorting 
-*/
+
+/**
+ * Find missing number without sorting
+ * 
+ * @param {array} arr 
+ * 
+ * @returns {number} missing num
+ */
 function findMissingNum2(arr) {
-  const n = arr.length + 1;
+    const n = arr.length + 1;
 
-  // formula for expected sum of arr length + 1
-  const expectedSum = n * (n + 1) / 2;
+    // Formula for expected sum of arr length + 1
+    const expectedSum = n * (n + 1) / 2;
 
-  // get actual sum of all elements in the arr
-  const actualSum = arr.reduce((a, b) => {
-    return a + b;
-  }, 0);
+    // Get actual sum of all elements in the arr
+    const actualSum = arr.reduce((a, b) => {
+        return a + b;
+    }, 0);
 
-  return expectedSum - actualSum;
+    return expectedSum - actualSum;
 }
 
