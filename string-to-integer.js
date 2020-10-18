0cdc3e07ec788972ef89796ac0d42b1fc38353ed
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 11:40:40 +0200

    transform string to integer

--- string-to-integer.js
+++ string-to-integer.js
@@ -1,13 +1,17 @@
-/*
-* transform string to integer
-*/
+/**
+ * Transform string to integer
+ * 
+ * @param {string} str
+ * 
+ * @returns {number} 
+ */
 function stringToInt(str) {
-  let sum = 0;
-  const arr = str.split('');
+    let sum = 0;
+    const arr = str.split('');
 
-  arr.map((currElement, index) => {
-    sum += currElement * Math.pow(10, arr.length - index - 1);
-  });
+    arr.map((currElement, index) => {
+        sum += currElement * Math.pow(10, arr.length - index - 1);
+    });
 
-  return sum;
+    return sum;
 }
