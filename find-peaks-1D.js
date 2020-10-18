a457995f3198df9579141b566a501a3d06bf33c2
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 09:57:50 +0200

    find peaks in array - 1D

--- find-peaks-1D.js
+++ find-peaks-1D.js
@@ -1,24 +1,31 @@
+/**
+ * Find peaks in an array
+ * 
+ * @param {array} arr 
+ * 
+ * @returns {array} peaks
+ */
 function findPeaks(arr) {
-  let peaks = [];
+    let peaks = [];
 
-  for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
 
-    // find if first element in the array is larger then next one
-    if (i === 0 && arr[i] >= arr[i+1] ) {
-      peaks.push(arr[i]);
+        // Find if first element in the array is larger then next one
+        if (i === 0 && arr[i] >= arr[i+1] ) {
+            peaks.push(arr[i]);
+        }
+
+        // Find other peaks in the array
+        if ( arr[i] >= arr[i+1] && arr[i] >= arr[i-1] ) {
+            peaks.push(arr[i]);
+        }
+
+        // Find if last element in the array is higher than previous one
+        if (i === arr.length - 1 && arr[i] >= arr[i-1]  ) {
+            peaks.push(arr[i]);
+        }
     }
 
-    // find other peaks in the array
-    if ( arr[i] >= arr[i+1] && arr[i] >= arr[i-1] ) {
-      peaks.push(arr[i]);
-    }
-
-    // find if last element in the array is higher than previous one
-    if (i === arr.length - 1 && arr[i] >= arr[i-1]  ) {
-      peaks.push(arr[i]);
-    }
-    
-  }
-  return peaks;
+    return peaks;
 }
 
