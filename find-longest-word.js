66f55184ca2d0ed2062d57ea955b272040d08426
DESKTOP-623QH5J\Tadej <tadej.fuks84@gmail.com>
2020-10-18 09:52:57 +0200

    find longest word

--- find-longest-word.js
+++ find-longest-word.js
@@ -1,21 +1,25 @@
-/*
-* find longest word in a sentence
-*/
+/**
+ * Find longest word
+ * 
+ * @param {string} str 
+ * 
+ * @returns {array} longest words
+ */
 function longestWord(str) {
 
-	// filter array for alpha numeric characters only
-	const words = str.toLowerCase().match(/[a-z0-9]+/g);
+    // Filter array for alpha numeric characters only
+    const words = str.toLowerCase().match(/[a-z0-9]+/g);
 
-	// sort words by length
-	const sorted = words.sort((a,b) => {
-		return b.length - a.length;
-	});
+    // Sort words by length
+    const sorted = words.sort((a,b) => {
+        return b.length - a.length;
+    });
 
-	// return one or more words with most characters
-	const longestWords = sorted.filter((word) => {
-		return word.length === sorted[0].length;
-	});
+    // Return one or more words with most characters
+    const longestWords = sorted.filter((word) => {
+        return word.length === sorted[0].length;
+    });
 
-	return longestWords;
+    return longestWords;
 }
 
